02b1a27a27f3c5769b698c118510a3f881ed9f90
Gabriel do Carmo <gabriel.desenvolvedor.97@gmail.com>
2026-09-06 19:17:22 -0300

    style(detekt): enforce strict detekt formatting rules, warningsAsErrors and 100% public KDoc

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,3 +12,7 @@ detekt {
     config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
     buildUponDefaultConfig = true
 }
+
+dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:${libs.versions.detekt.get()}")
+}
